6da6b35c6a1deace2c35bfbed5d7a078ccaf6a99
SimonTannert <st103267@stud.uni-stuttgart.de>
2019-01-13 17:21:02 +0100

    changed max tree depth from 5 to 3

--- lib/classifiers/decision_tree.py
+++ lib/classifiers/decision_tree.py
@@ -1,6 +1,6 @@
 from sklearn import tree
 
-def train(x, y, max_depth=5):
+def train(x, y, max_depth=3):
     clf = tree.DecisionTreeClassifier(max_depth=max_depth)
     clf.fit(x, y)
     return clf
