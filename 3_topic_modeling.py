75a3a4e93974e49500157f67ac9a2d525af37843
SimonTannert <st103267@stud.uni-stuttgart.de>
2019-01-13 13:42:24 +0100

    lda WIP

--- 3_topic_modeling.py
+++ 3_topic_modeling.py
@@ -6,14 +6,36 @@ Usage:
 
 import docopt
 import gensim
+from gensim.corpora.dictionary import Dictionary
 import nltk
 
 from lib.elastic import og_reader
 
 
+dictionary = Dictionary()
+
+
+def extract_text(tweet):
+    try:
+        text = tweet['_source']['extended_tweet']['full_text']
+    except KeyError:
+        try:
+            text = tweet['_source']['extended_tweet']['text']
+        except KeyError:
+            try:
+                text = tweet['_source']['message']
+            except KeyError:
+                try:
+                    text = tweet['_source']['text']
+                except KeyError:
+                    text = None
+    tokens = nltk.word_tokenize(tweet)
+    return dictionary.doc2bow(tokens)
+
+
 def main(num_topics):
     with og_reader.get_reader() as r:
-        model = gensim.models.LdaMulticore(r, num_topics=num_topics)
+        model = gensim.models.LdaMulticore((extract_text(x) for x in r), num_topics=num_topics)
     return model
 
 
